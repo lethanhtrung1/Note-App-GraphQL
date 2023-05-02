d560f0c9e4488470330c24d633a09deadfaf8476
Le Thanh Trung <trunglt1011@gmail.com>
2023-05-02 23:33:12 +0700

    add component list folder in home page

---
+++ client/src/components/FolderList.jsx
@@ -0,0 +1,44 @@
+import { Card, CardContent, List, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function FolderList({folders}) {
+    return (
+        <List 
+            sx={{
+                width: '100%',
+                height: '100%',
+                padding: '10px',
+                backgroundColor: '#7D9D9C',
+                textAlign: 'left',
+                overflowY: 'auto'
+            }}
+            subheader={
+                <Box>
+                    <Typography sx={{ fontWeight: 'bold', color: 'white' }}>
+                        Folder
+                    </Typography>
+                </Box>
+            }
+        >
+            {
+                folders.map(({ id, name }) => {
+                    return (
+                        <Link
+                            key={id}
+                            to={`folders/${id}`}
+                            style={{ textDecoration: 'none' }}
+                        >
+                            <Card sx={{ marginBottom: '5px' }}>
+                                <CardContent sx={{ '&:last-child': { paddingBottom: '10px' }, padding: '10px' }}>
+                                    <Typography>{name}</Typography>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    )
+                })
+            }
+        </List>
+    )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { Box } from '@mui/system';
 import UserMenu from '../components/UserMenu'; 
+import FolderList from "../components/FolderList";
 
 export default function Home() {
     return (
@@ -10,6 +11,19 @@ export default function Home() {
             <Box sx={{ display: 'flex', justifyContent: 'right', marginBottom: '10px' }}>
                 <UserMenu />
             </Box>
+
+            <Grid container sx={{ height: '50vh', boxShadow: '0 0 15px 0 rgb(193 193 193 / 60%)'}}>
+                <Grid item xs={3} sx={{ height: '100%' }}>
+                    <FolderList folders={[ 
+                        { id: '1', name: 'Lập trình web' },
+                        { id: '2', name: 'Thực tập cơ sở'},
+                        { id: '3', name: 'Cơ sở dữ liệu phân tán'},
+                    ]} />
+                </Grid>
+                <Grid item xs={9} sx={{ height: '100%' }}>
+                    <p>Node List</p>
+                </Grid>
+            </Grid>
         </>
     )
 }
