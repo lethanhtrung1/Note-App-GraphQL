b677af94c347b9141d3cda1e3136ca825f240724
Le Thanh Trung <trunglt1011@gmail.com>
2023-05-03 21:48:23 +0700

    Create component Note using react-draft-wysiwyg & draftjs

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ContentState, EditorState, convertFromHTML, convertToRaw } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import draftToHtml from "draftjs-to-html";
 
 export default function Note() {
     const note = {
@@ -6,5 +9,31 @@ export default function Note() {
         content: "<p>New Note content</p>",
     };
 
-    return <div>Note123</div>;
+    const [editorState, setEditorState] = useState(() => {
+        return EditorState.createEmpty();
+    });
+    const [rawHTML, setRawHTML] = useState(note.content);
+
+    useEffect(() => {
+        const blockFromHTML = convertFromHTML(note.content);
+        const state = ContentState.createFromBlockArray(blockFromHTML.contentBlocks, blockFromHTML.entityMap);
+        setEditorState(EditorState.createWithContent(state));
+    }, [note.id]);
+
+    useEffect(() => {
+        setRawHTML(note.content);
+    }, [note.content]);
+
+    const handleOnChange = (e) => {
+        setEditorState(e);
+        setRawHTML(draftToHtml(convertToRaw(e.getCurrentContent())));
+    };
+
+    return (
+        <Editor 
+            editorState={editorState}
+            onEditorStateChange={handleOnChange} 
+            placeholder="Write something!" 
+        />
+    );
 }
